207eeb881bc5b19da0157a02abbc3b85ddb153dd
Rohit Apte <rohit@rohitapte.com>
2018-03-27 00:24:48 +0800

    generate text file from wikipedia directories

--- ExtractTextFromWikipediaDataDump.py
+++ ExtractTextFromWikipediaDataDump.py
@@ -1,34 +1,30 @@
-import xml.etree.ElementTree as etree
 import os
 import re
 
-PATH_WIKI_XML='C:\\Users\\tihor\\Documents\\'
-FILENAME_WIKI='enwiki-latest-pages-articles-multistream.xml'
+INPUT_DIR='C:/Users/tihor/Documents/wiki_text'
+OUTPUT_PATH='C:/Users/tihor/Documents'
+OUTPUT_FILE='text_corpus_cased.txt'
 ENCODING="utf-8"
+LOWERCASE=False
 
-pathWikiXML=os.path.join(PATH_WIKI_XML, FILENAME_WIKI)
+remove_space_comma=re.compile(r' ,')
+file_count=0
 
-regex_expressions=[]
-regex_expressions.append((re.compile('\s\s+'),' '))
-regex_expressions.append((re.compile('<ref.*?<\/ref>'),''))
-regex_expressions.append((re.compile('\{\{.*?\}\}'),''))
-regex_expressions.append((re.compile('\[\[(?:[^|\]]*\|)'),''))
-regex_expressions.append((re.compile('\[\['),''))
-regex_expressions.append((re.compile('\]\]'),''))
-regex_expressions.append((re.compile("'''"),"'"))
-regex_expressions.append((re.compile("''"),"'"))
-regex_expressions.append((re.compile(" ,"),","))
-regex_expressions.append((re.compile('\=\=(?:[^|\]]*\=\=)'),''))
-
-
-totalCount=0
-
-
-for event, elem in etree.iterparse(pathWikiXML, events=('start', 'end')):
-    if elem.tag=='{http://www.mediawiki.org/xml/export-0.10/}text' and elem.text is not None and event=='end':
-        article_text=elem.text
-        if article_text[:10]!='#REDIRECT ':
-            for item,replace_item in regex_expressions:
-                article_text=item.sub(replace_item,article_text).strip()
-            x=-5
+with open(os.path.join(os.path.normpath(OUTPUT_PATH),OUTPUT_FILE),"w",encoding=ENCODING) as f_out:
+    for dirpath,dirnames,filenames in os.walk(os.path.normpath(INPUT_DIR)):
+        for file in filenames:
+            file_count+=1
+            with open(os.path.join(dirpath,file),'r',encoding=ENCODING) as f_in:
+                content=f_in.readlines()
+                for item in content:
+                    tag_check=item[0:5]
+                    if tag_check!='<doc ' and tag_check!='</doc':
+                        text=item.replace('<nowiki>*</nowiki>','')
+                        text=remove_space_comma.sub(' ',text).strip()
+                        if LOWERCASE:
+                            text=text.lower()
+                        if len(text)>0:
+                            f_out.write(text+'\n')
+            if file_count>1000:
+                break
 
